812a05d89d9aaa7873d5532104c05da069c6d1ce
Brian Charous <brian.charous@gmail.com>
2015-01-10 15:34:51 -0600

    answer email questions

--- pal/services/directory_service.py
+++ pal/services/directory_service.py
@@ -18,11 +18,11 @@ class DirectoryService(AbstractService):
         return 1
 
     def go(self, features):
-
+        # print features
         directory = Directory()
 
-        # pull the person's record out of the database
         nouns = features['nouns']
+        keywords = features['keywords']
         full_names = [x for x in nouns if x[1] == 'PERSON']
 
         if len(full_names) == 1:
@@ -38,7 +38,7 @@ class DirectoryService(AbstractService):
 
             # answer where a student lives
             room_keywords = set(['room', 'dorm', 'house', 'hall'])
-            if len(room_keywords.intersection(set(noun_words))) > 0:
+            if len(room_keywords.intersection(set(noun_words))) or len(room_keywords.intersection(set(keywords))) > 0:
                 matching_students = matching_people['students']
                 if len(matching_students) > 1:
                     raise NotEnoughInformationException(
@@ -50,7 +50,7 @@ class DirectoryService(AbstractService):
 
             # answer where a faculty/staff office is
             office_keywords = set(['office'])
-            if len(office_keywords.intersection(set(noun_words))) > 0:
+            if len(office_keywords.intersection(set(noun_words))) or len(room_keywords.intersection(set(keywords))) > 0:
                 matching_facstaff = matching_people['facstaff']
                 if len(matching_facstaff) > 1:
                     raise NotEnoughInformationException(
@@ -61,7 +61,50 @@ class DirectoryService(AbstractService):
                 facstaff = matching_facstaff[0]
                 building_name = directory.get_name_for_id(
                     Building, facstaff.office_building_id)
-                return {'response': "{0} {1} works in {2} {3}".format(first_name, last_name, building_name, facstaff.office)}
+                return {'response': "{0} {1}'s office is {2} {3}".format(first_name, last_name, building_name, facstaff.office)}
+
+            # answer phone numbers
+            phone_keywords = set(['phone', 'number', 'call'])
+            if len(phone_keywords.intersection(noun_words)) > 0 or len(phone_keywords.intersection(set(keywords))):
+                phones = []
+                if len(matching_people['students']) > 0 and len(matching_people['facstaff']) > 0:
+                    raise NotEnoughInformationException(
+                        "Too many matches found for name: {0} {1}".format(first_name, last_name))
+                elif len(matching_people['students']) > 0:
+                    phones = matching_people['students'][0].phones
+                elif len(matching_people['facstaff']) > 0:
+                    phones = matching_people['facstaff'][0].phones
+                else:
+                    raise NotEnoughInformationException(
+                        "Could not find {0} {1}".format(first_name, last_name))
+
+                if len(phones) == 0:
+                    return {'response': "{0} {1} doesn't seem to have any phone numbers listed".format(first_name, last_name)}
+                elif len(phones) == 1:
+                    return {'response': "{0} {1}'s phone number is {2}".format(first_name, last_name, phones[0])}
+                else:
+                    return{'response': "{0} {1}'s phone numbers are {2}".format(first_name, last_name, ', '.join([str(phone) for phone in phones]))}
+
+
+            # answer email questions
+            email_keywords = set(['email', 'e-mail', 'mail'])
+            if len(email_keywords.intersection(noun_words)) > 0 or len(email_keywords.intersection(set(keywords))):
+                email = ''
+                if len(matching_people['students']) > 0 and len(matching_people['facstaff']) > 0:
+                    raise NotEnoughInformationException(
+                        "Too many matches found for name: {0} {1}".format(first_name, last_name))
+                elif len(matching_people['students']) > 0:
+                    email = matching_people['students'][0].email
+                elif len(matching_people['facstaff']) > 0:
+                    email = matching_people['facstaff'][0].email
+                else:
+                    raise NotEnoughInformationException(
+                        "Could not find {0} {1}".format(first_name, last_name))
+
+                if email == '':
+                    return {'response': "{0} {1} doesn't seem to an email address listed".format(first_name, last_name)}
+                else:
+                    return {'response': "{0} {1}'s email address is {2}".format(first_name, last_name, email)}
 
         else:
             # these types of questions pertain to 2 or more people
