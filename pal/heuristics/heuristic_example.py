0467ec5a3f95b50c23671abb60c83ab74f3261c9
Greg Fournier <fournieg@carleton.edu>
2015-01-16 20:14:39 -0600

    Now edits a text file with hill climbing results, not print to screen
    Conflicts:
    	pal/heuristics/heuristic_example.py

--- pal/heuristics/heuristic_example.py
+++ pal/heuristics/heuristic_example.py
@@ -11,22 +11,15 @@ from pal.heuristics.movie_heuristic import MovieHeuristic
 #     movie_heuristic.movieHeuristic()
 #     if curValues > best:
 #         best = cur
-# asdf
 
 dummyDict = {'keywords': ['Movie', 'actor'], 'Proper Nouns': ['Tom Hanks']}
 dummyEVILDict = {'keywords': ['time', 'theatre'],
                  'Proper Nouns': ['SATAN', 'Satin']}
 
+
 myHeuristic = MovieHeuristic()
-temp = hill_climb([dummyDict], [dummyEVILDict], 50, [75, 75, 75, 50, 60, 40,
-                                                     60, 50, 50, 40, 40, 50,
-                                                     -50, -50, -75, -60, -60,
-                                                     -90, -25, 30],
-                  myHeuristic)
+keywords = myHeuristic.get_input_list_keywords()
 
-print temp[0].get_magnitudes()
-
-temp = hill_climb([dummyDict], [dummyEVILDict], 50, temp[0].get_magnitudes(),
-                  myHeuristic)
-
-print temp[0].get_magnitudes()
+hill_climb([dummyDict], [dummyEVILDict], 100,
+           myHeuristic.get_input_list_values(), myHeuristic,
+           keywords, 'climbed_movie_values.txt')
