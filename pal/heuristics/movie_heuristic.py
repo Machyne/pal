5ce8aec205958f57717983d4e488fc239dc81ed2
Greg Fournier <fournieg@carleton.edu>
2015-01-16 20:15:50 -0600

    Added get_input_list_values and get_input_list_keywords

--- pal/heuristics/movie_heuristic.py
+++ pal/heuristics/movie_heuristic.py
@@ -10,11 +10,11 @@ class MovieHeuristic(Heuristic):
 
     # Returns a heuristic value for a list of keywords
     def run_heuristic(self, keywords):
-        toBeReturned = 0
+        to_be_returned = 0
         for word in keywords:
             if word in self.input_list:
-                toBeReturned += self.input_list[word]
-        return toBeReturned
+                to_be_returned += self.input_list[word]
+        return to_be_returned
 
     def read_input_file(self):
         # read input file into dictionary with keyword as key and
@@ -23,6 +23,19 @@ class MovieHeuristic(Heuristic):
         for line in input_file:
             cur_line = line.split(',')
             self.input_list += [(cur_line[0], int(cur_line[1]))]
+
+    def get_input_list_values(self):
+        to_be_returned = []
+        for item in self.input_list:
+            to_be_returned += [item[1]]
+        return to_be_returned
+
+    def get_input_list_keywords(self):
+        to_be_returned = []
+        for item in self.input_list:
+            to_be_returned += [item[0]]
+        return to_be_returned
+
 
 if __name__ == '__main__':
     my_heuristic = MovieHeuristic()
