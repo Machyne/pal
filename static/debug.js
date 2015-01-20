acae8d2e48a19fa9687dacf0942337141e7ca3ab
Ken Schiller <kenschiller@gmail.com>
2015-01-20 09:50:25 -0600

    Implement debug client

---
+++ static/debug.js
@@ -0,0 +1,59 @@
+var queryPAL = function(query, callback) {
+  $.ajax({
+    type: 'POST',
+    url: '/api/pal',
+    data: {
+      query: query,
+      client: 'web'
+    },
+    success: function (response) {
+      callback(response.result.response);
+    }
+  });
+};
+
+var queryEndpoint = function(query, endpoint) {
+  $.ajax({
+    type: 'POST',
+    url: '/api/' + endpoint,
+    data: {
+      query: query,
+      client: 'web'
+    },
+    success: function (response) {
+      $('.' + endpoint + ' textarea').val(JSON.stringify(response));
+      console.log(JSON.stringify(response));
+    }
+  });
+}
+
+$(document).ready(function () {
+
+  var showResult = function (result) {
+        $('.history-result').append('<li>' + result + '</li>');
+      },
+      prompt = $('.prompt'),
+      sendQuery = function () {
+        var query = prompt.val();
+        if (query.length > 0) {
+          queryPAL(query, showResult);
+          queryEndpoint(query, 'standard_nlp');
+          queryEndpoint(query, 'features');
+          queryEndpoint(query, 'classify');
+          queryEndpoint(query, 'execute');
+          $('.history-prompt').append('<li>' + query + '</li>');
+        }
+      };
+
+  prompt.focus();
+
+  prompt.on('keypress', function (e) {
+    // 'enter' key
+    if (e.which == 13) {
+      sendQuery();
+    }
+  }).on('change', sendQuery);
+
+  $('#go-btn').on('click', sendQuery);
+
+});
