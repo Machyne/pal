69ef165c57032629bb8d96b4d92efec5e4e6bdbe
Alex Simonides <acsimonides@gmail.com>
2015-01-25 13:24:21 -0600

    Sketch out UL service and add heuristic file

---
+++ pal/services/ultralingua_service.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# A service for translations
+
+import requests
+
+from pal.services.service import Service
+
+
+API_URL = ("http://api.ultralingua.com/api/definitions/{from_}/{to_}/{word}"
+           "?token=palrocks")
+
+
+class UltraLinguaService(Service):
+
+    _SUPPORTED_LANGUAGES = {"english", "french", "spanish",
+                            "german", "portuguese", "italian"}
+    _SPECIAL_ISO_CODES = {
+        # these are the only 2 that aren't just the first 3
+        # letters of the english name
+        "french": "fra",
+        "german": "deu"
+    }
+
+    def applies_to_me(self, client, feature_request_type):
+        return True
+
+    def get_confidence(self, features):
+        pass
+
+    @classmethod
+    def _get_iso_code(cls, language):
+        """ Returns the appropriate 3-letter ISO code for the
+            given language.
+        """
+        iso = cls._SPECIAL_ISO_CODES.get(language, language[:3])
+        return iso if language in cls._SUPPORTED_LANGUAGES else None
+
+    def go(self, features):
+        tokens = map(str.lower, features['tokens'])
+        keywords = set(features['keywords'])
+        from_lang = "english"
+        to_lang = None
+        if tokens:
+            the_word = None
+            languages = self._SUPPORTED_LANGUAGES.intersection(  # NOQA
+                keywords)
+            # If there's more than one language, figure out which is from_lang
+            # and which is to_lang
+            #
+            # If tokens looks like ["translate", ... "to", <language>], take
+            # the middle as the_word
+
+            url = API_URL.format(from_=from_lang, to_=to_lang, word=the_word)
+            response = requests.get(url)  # NOQA
+            # parse the response from something that looks like:
+            # response = [{
+            #   "partofspeech": {
+            #     "gender": "feminine",
+            #     "number": "singular",
+            #     "partofspeechcategory": "noun",
+            #     "partofspeechdisplay": "noun"
+            #   },
+            #   "root": "chaise",
+            #   "surfaceform": "chaise",
+            #   "text": "chaise"
+            # }, {
+            #   "partofspeech": {
+            #     "gender": "masculine",
+            #     "number": "singular",
+            #     "partofspeechcategory": "noun",
+            #     "partofspeechdisplay": "noun"
+            #   },
+            #   "root": "fauteuil",
+            #   "surfaceform": "fauteuil",
+            #   "text": "fauteuil"
+            # }, {
+            #   "partofspeech": {
+            #     "gender": "masculine",
+            #     "number": "singular",
+            #     "partofspeechcategory": "noun",
+            #     "partofspeechdisplay": "noun"
+            #   },
+            #   "root": "si\u00e8ge",
+            #   "surfaceform": "si\u00e8ge",
+            #   "text": "si\u00e8ge"
+            # }, {
+            #   "clarification": ["chair a panel"],
+            #   "partofspeech": {
+            #     "partofspeechcategory": "verb",
+            #     "partofspeechdisplay": "verb",
+            #     "tense": "infinitive"
+            #   },
+            #   "root": "pr\u00e9sider",
+            #   "surfaceform": "pr\u00e9sider",
+            #   "text": "pr\u00e9sider"
+            # }]
+
+            # Return cleaned response
+
+        return None
