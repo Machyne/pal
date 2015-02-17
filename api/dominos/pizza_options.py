c0a6670499cc4aee1f43f32b06f8972c7732726c
Matt Cotter <mcotter222@gmail.com>
2015-02-16 23:21:09 -0600

    add full pizza api

---
+++ api/dominos/pizza_options.py
@@ -0,0 +1,163 @@
+import json
+
+import requests
+
+TOPPINGS = {
+    # Cheese
+    'C': 'cheese',
+    # Sauce
+    'X': ('robust inspired tomato sauce', 'tomato sauce', 'sauce'),
+    'Xm': 'marinara sauce',
+    'Xw': 'white sauce',
+    'Bq': ('barbecue sauce', 'bbq sauce'),
+    # Meats
+    'P': 'pepperoni',
+    'S': ('italian sausage', 'sausage'),
+    'Sb': ('sliced italian sausage', 'sliced sausage'),
+    'B': 'beef',
+    'Pm': ('philly steak', 'steak'),
+    'H': 'ham',
+    'K': 'bacon',
+    'Sa': 'salami',
+    'Du': ('premium chicken', 'chicken'),
+    # Cheese Toppings
+    'E': ('cheddar cheese', 'cheddar'),
+    'Fe': ('feta cheese', 'feta'),
+    'Cs': ('shredded parmesan asiago', 'parmesan', 'asiago'),
+    'Cp': ('shredded provolone cheese', 'provolone'),
+    # Veggies / Fruit / Other
+    'Z': 'banana peppers',
+    'R': 'black olives',
+    'V': ('green olives', 'olives'),
+    'G': ('green pepper', 'pepper'),
+    'J': ('jalapeno peppers', 'jalapenos', 'jalapeno'),
+    'M': 'mushrooms',
+    'N': 'pineapple',
+    'O': ('onions', 'onion'),
+    'Rr': ('roasted red pepper', 'red pepper'),
+    'Si': 'spinach',
+    'Td': ('diced tomatoes', 'tomatoes', 'tomato'),
+    'Ht': 'hot sauce',
+}
+
+# (names): (format, (size_options))
+CRUSTS = {
+    ('hand tossed', 'regular'): ("{}SCREEN", (10, 12, 14, 16)),
+    ('pan', 'handmade pan'): ("P{}IPAZA", (10,)),
+    ('thin', 'crunchy thin'): ("{}THIN", (10, 12, 14)),
+    ('brooklyn', 'brooklyn style'): ("PBKERIZA", (14,)),
+    ('gfree', 'gluten free'): ("P{}IGFZA", (10,)),
+}
+
+SIZES = {
+    'small': 10,
+    'medium': 12,
+    'large': 14,
+    'xlarge': 16,
+    10: 10,
+    12: 12,
+    14: 14,
+    16: 16,
+}
+
+AMOUNTS = {
+    'no': 0.0,
+    'none': 0.0,
+    'light': 0.5,
+    'normal': 1.0,
+    'extra': 1.5,
+    'double': 2.0,
+    'triple': 3.0,
+    0.0: 0.0,
+    0.5: 0.5,
+    1.0: 1.0,
+    1.5: 1.5,
+    2.0: 2.0,
+    3.0: 3.0,
+}
+
+
+def _topping_amt(full=None, left=None, right=None):
+    if sum(AMOUNTS[x or 0] for x in (full, left, right)) == 0:
+        return 0
+    if full is not None:
+        return {'1/1': AMOUNTS[full]}
+    left, right = left or 0, right or 0
+    return {'1/2': AMOUNTS[left], '2/2': AMOUNTS[right]}
+
+
+def _get_crust(size, crust):
+    crust = crust.lower()
+    if isinstance(size, str):
+        size = size.lower()
+    for names in CRUSTS:
+        if crust in names:
+            format_str, sizes = CRUSTS[names]
+            size = SIZES[size]
+            if size in sizes:
+                return format_str.format(size)
+            else:
+                return format_str.format(sizes[0])
+    return '12SCREEN'
+
+
+def _topping_code(topping):
+    for code, names in TOPPINGS.iteritems():
+        if topping in names:
+            return code
+    return topping
+
+
+def order_pizza(phone, name, address, card, pizzas):
+    name_parts = name.rpartition(' ')
+    data = {
+        'phone': phone,
+        'firstName': name_parts[0] or name_parts[-1],
+        'lastName': name_parts[-1],
+        'address': address,
+        'card': card,
+        'pizzas': [],
+    }
+    for pizza in pizzas:
+        pie = {'options': {}}
+        size, _, crust_type = pizza['crust'].partition(' ')
+        pie['crust'] = _get_crust(size, crust_type)
+        pie['quantity'] = pizza['quantity']
+        for topping, amount in pizza['options']:
+            print topping, amount
+            if not isinstance(amount, dict):
+                amount = {'full': amount}
+            pie['options'][_topping_code(topping)] = _topping_amt(**amount)
+            print pie['options']
+        data['pizzas'].append(pie)
+
+    print data['pizzas'][0]
+    raw_input()
+    data = json.dumps(data)
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post("http://localhost:8000", data=data, headers=headers)
+    return r.json()
+
+
+if __name__ == '__main__':
+    card = {
+        'num': "4444888888888888",
+        'type': "VISA",
+        'expire': "1017",
+        'cvv': "189",
+        'zip': "55057",
+    }
+    phone = '2024561111'
+    name = 'Test Tester'
+    address = '1 N College St, Northfield, MN 55057'
+    pizza = {
+        'crust': 'medium thin',
+        'quantity': 1,
+        'options': [
+            ('cheese', 'no'),
+            ('sauce', 'extra'),
+            ('mushrooms', 'normal'),
+            ('jalapeno', 'normal'),
+        ],
+    }
+    print order_pizza(phone, name, address, card, [pizza])
--- api/dominos/server.js
+++ api/dominos/server.js
@@ -1,5 +1,4 @@
 var http = require('http')
-  , querystring = require('querystring')
   , dominos = require('dominos');
 
 var processReq = function (req, res, callback) {
@@ -14,10 +13,12 @@ var processReq = function (req, res, callback) {
       }
     });
     req.on('end', function() {
-      callback(req, res, querystring.parse(queryData));
+      if (typeof(queryData) !== typeof({})) {
+        queryData = JSON.parse(queryData);
+      };
+      callback(req, res, queryData);
     });
   } else {
-    // Only allow posts.
     res.writeHead(405, {'Content-Type': 'text/plain'});
     res.end();
   }
@@ -26,12 +27,12 @@ var processReq = function (req, res, callback) {
 var dominosHandler = function (req, res, post) {
   console.log(post);
   // Handle needed top level parameters.
-  var need = ['phone', 'firstName', 'lastName', 'email', 'address', 'card'];
+  var need = ['phone', 'firstName', 'lastName', 'address', 'card'];
   for (var i = 0; i < need.length; i++) {
     var key = need[i];
     if (!post.hasOwnProperty(key)) {
       res.writeHead(400, {'Content-Type': 'text/plain'});
-      return res.end('Need key `' + key + '`.');
+      return res.end(JSON.stringify({msg: 'Need key `' + key + '`.'}));
     };
   };
 
@@ -39,19 +40,98 @@ var dominosHandler = function (req, res, post) {
   var need_card = ['num', 'type', 'expire', 'cvv', 'zip'];
   for (var i = 0; i < need_card.length; i++) {
     var key = need_card[i];
-    if (!post.card.hasOwnProperty(key)) {
+    if (!(key in post.card)) {
       res.writeHead(400, {'Content-Type': 'text/plain'});
-      return res.end('Need key `card.' + key + '`.');
+      return res.end(JSON.stringify({msg: 'Need key `card.' + key + '`.'}));
     };
   };
 
-  // TODO handle actual order.
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Pizza complete.');
+  placeOrder(post, function (err) {
+    if (err) {
+      res.writeHead(400, {'Content-Type': 'application/JSON'});
+      res.end(JSON.stringify(err));
+    }
+    res.writeHead(200, {'Content-Type': 'application/JSON'});
+    res.end(JSON.stringify({msg: 'Pizza complete.'}));
+  });
 };
 
 http.createServer(function(req, res) {
   processReq(req, res, dominosHandler);
 }).listen(8000);
 console.log('Server listening on port 8000.');
+
+var placeOrder = function (data, callback) {
+  var order = new dominos.class.Order();
+  order.Order.Phone = data.phone;
+  order.Order.FirstName = data.firstName;
+  order.Order.LastName = data.lastName;
+  order.Order.Email = '';
+
+  dominos.store.find(data.address, function (storeData) {
+    if (!storeData.success) {
+      console.log('No store found');
+      return callback({msg: 'no stores found'});
+    };
+    order.Order.Address = storeData.result.Address;
+    order.Order.StoreID = storeData.result.Stores[0].StoreID;
+
+    for (var i = 0; i < data.pizzas.length; i++) {
+      var pizza = data.pizzas[i];
+      order.Order.Products.push({
+        Code: pizza.crust,
+        ID: 1,
+        Options: pizza.options,
+        Qty: pizza.quantity,
+        isNew: true
+      })
+    };
+
+    dominos.order.validate(order, function (orderData) {
+      if (!orderData.success) {
+        console.log('Order could not be validated');
+        return callback({msg: 'cannot validate order'});
+      };
+
+      dominos.order.price(orderData.result, function (priceData) {
+
+        var cardInfo = new dominos.class.Payment();
+        cardInfo.Amount = priceData.result.Order.Amounts.Customer;
+
+        cardInfo.Number = data.card.num;
+        cardInfo.CardType = data.card.type;
+        cardInfo.Expiration = data.card.expire;
+        cardInfo.SecurityCode = data.card.cvv;
+        cardInfo.PostalCode = data.card.zip;
+
+        order.Order.Payments.push(cardInfo);
+
+        console.log('Total cost:', cardInfo.Amount);
+
+        // Place order
+        dominos.order.place(order, function (orderData) {
+          var curOrder = orderData.result.Order;
+          console.log('Order placed:\n', curOrder);
+          if (curOrder.Status < 0) {
+              var details = JSON.stringify(curOrder.StatusItems) +
+                '\n' +
+                JSON.stringify(curOrder.CorrectiveAction);
+              console.log(
+                  '\n######### ERROR! ########\n',
+                  details,
+                  '\n#########################\n\n'
+              );
+              var msg = curOrder.CorrectiveAction.Detail + ': ' +
+                curOrder.CorrectiveAction.Code;
+              return callback({msg: msg, details: details});
+          }
+
+          dominos.track.phone(orderData.result.Order.Phone, function (pizzaData) {
+            console.log('Tracking data: ', pizzaData);
+            return callback();
+          });
+        });
+      });
+    });
+  });
+};
