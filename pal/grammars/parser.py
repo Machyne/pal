8ea89554cbcc7c4301c28eeedf2f7c98350c3995
Ken Schiller <kenschiller@gmail.com>
2015-02-23 01:06:01 -0600

    Fix CYK parse on unit productions

--- pal/grammars/parser.py
+++ pal/grammars/parser.py
@@ -1,7 +1,6 @@
 import pprint
 import re
 import sys
-from collections import defaultdict
 
 from pal.grammars.grammars import make_chomsky_normal_form
 from pal.grammars.grammars import parse_grammar_from_file
@@ -29,7 +28,8 @@ def main():
 def generate_grammar_features(grammar):
     key_list = sorted([key for key in grammar.iterkeys() if not key == '$'])
     terminal_rules = []
-    non_terminal_rules = []
+    unit_rules = []
+    pair_rules = []
     for i, key in enumerate(key_list):
         rules = grammar[key]
         for rule in rules:
@@ -38,13 +38,13 @@ def generate_grammar_features(grammar):
             elif isinstance(rule, tuple):
                 b = key_list.index(rule[0])
                 if len(rule) == 1:
-                    non_terminal_rules.append((i, b))
+                    unit_rules.append((i, b))
                 if len(rule) == 2:
                     c = key_list.index(rule[1])
-                    non_terminal_rules.append((i, b, c))
+                    pair_rules.append((i, b, c))
     lexicon = [rule[1] for rule in terminal_rules]
     start_key = key_list.index(grammar['$'])
-    return key_list, terminal_rules, non_terminal_rules, lexicon, start_key
+    return key_list, terminal_rules, unit_rules, pair_rules, lexicon, start_key
 
 
 def preprocess(string, grammar_features):
@@ -57,7 +57,7 @@ def preprocess(string, grammar_features):
             return '#'
         except ValueError:
             return '*'
-    lexicon = grammar_features[3]
+    lexicon = grammar_features[4]
     words = [word if word in lexicon else wild_card(word)
              for word in string.split()]
     string = ' '.join(words)
@@ -70,47 +70,46 @@ def parse(string, grammar_features):
 
         Note: Expects a lowercase string without ending punctuation.
     """
-    (key_list, terminal_rules, non_terminal_rules,
+    (key_list, terminal_rules, unit_rules, pair_rules,
         lexicon, start_key) = grammar_features
     original_words = string.split()
     string = preprocess(string, grammar_features)
     words = string.split()
     words_count = len(words)
-    p = defaultdict(list)
+    p = {}
     for i in xrange(words_count):
         for rule in terminal_rules:
             j, rhs = rule
             if rhs == words[i]:
-                p[(0, i, j)] = [key_list[j], original_words[i]]
-        changed = 1
+                p[(0, i, j)] = (key_list[j], original_words[i])
+        changed = True
         while changed:
-            changed = 0
-            for rule in non_terminal_rules:
-                if len(rule) == 2:
-                    a, b = rule
-                    if p[(0, i, b)] and not p[(0, i, a)]:
-                        p[(0, i, a)] = [key_list[a], (p[(0, i, b)],)]
-                        changed += 1
-
+            changed = False
+            for rule in unit_rules:
+                a, b = rule
+                if (0, i, b) in p and (0, i, a) not in p:
+                    p[(0, i, a)] = (key_list[a], (p[(0, i, b)],))
+                    changed = True
     for i in xrange(1, words_count):
         for j in xrange(words_count - i):
             for k in xrange(i):
-                for rule in non_terminal_rules:
-                    if len(rule) == 3:
-                        a, b, c = rule
-                        if p[(k, j, b)] and p[(i - k - 1, j + k + 1, c)]:
-                            p[(i, j, a)] = [key_list[a], (p[(k, j, b)],
-                                            p[(i - k - 1, j + k + 1, c)])]
-            changed = 1
+                for rule in pair_rules:
+                    a, b, c = rule
+                    if (k, j, b) in p and (i - k - 1, j + k + 1, c) in p:
+                        p[(i, j, a)] = (key_list[a], (p[(k, j, b)],
+                                        p[(i - k - 1, j + k + 1, c)]))
+            changed = True
             while changed:
-                changed = 0
-                for rule in non_terminal_rules:
-                    if len(rule) == 2:
-                        a, b = rule
-                        if p[(i, j, b)] and not p[(i, j, a)]:
-                            p[(i, j, a)] = [key_list[a], (p[(0, i, b)],)]
-                            changed += 1
-    return p[(words_count - 1, 0, start_key)]
+                changed = False
+                for rule in unit_rules:
+                    a, b = rule
+                    if (i, j, b) in p and (i, j, a) not in p:
+                        p[(i, j, a)] = (key_list[a], (p[(i, j, b)],))
+                        changed = True
+    if (words_count - 1, 0, start_key) in p:
+        return p[(words_count - 1, 0, start_key)]
+    else:
+        return False
 
 
 def extract(symbol, parse_tree):
@@ -120,8 +119,11 @@ def extract(symbol, parse_tree):
     def flatten(parse_tree):
         key, value = parse_tree
         if isinstance(value, tuple):
-            left, right = value
-            return flatten(left) + flatten(right)
+            if len(value) == 1:
+                return flatten(value[0])
+            else:
+                left, right = value
+                return flatten(left) + flatten(right)
         else:
             return [value]
 
@@ -131,8 +133,11 @@ def extract(symbol, parse_tree):
         return ' '.join(words)
     else:
         if isinstance(value, tuple):
-            left, right = value
-            return extract(symbol, left) or extract(symbol, right) or ''
+            if len(value) == 1:
+                return extract(symbol, value[0]) or ''
+            else:
+                left, right = value
+                return extract(symbol, left) or extract(symbol, right) or ''
         return ''
 
 
