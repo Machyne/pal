08c2a58ecabbd550f51e69c46639e9a64a3d4894
Matt Cotter <mcotter222@gmail.com>
2015-02-25 17:41:26 -0600

    removed unused js function

--- static/home.js
+++ static/home.js
@@ -1,22 +1,3 @@
-function SelectText(element) {
-    //Taken from http://stackoverflow.com/questions/985272/selecting-text-in-an-element-akin-to-highlighting-with-your-mouse
-    var doc = document
-        , text = doc.getElementById(element)
-        , range, selection
-    ;
-    if (doc.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(text);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();
-        range = document.createRange();
-        range.selectNodeContents(text);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-}
-
 var resetQueryBar = function () {
   $('#prompt').removeAttr('disabled');
   $('#go-btn').removeAttr('disabled');
