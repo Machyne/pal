6adbf4ed1a00a8e7f913afb23c17d88576216157
Alex Simonides <acsimonides@gmail.com>
2015-03-09 21:02:06 -0500

    Add license to api files and hooks`

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# coding: utf-8
+#
+# Copyright (c) 2015, PAL Team.
+# All rights reserved. See LICENSE for details.
 #
 # Module for APIs created by the PAL team
 #
--- api/dominos/pizza_options.py
+++ api/dominos/pizza_options.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Copyright (c) 2015, PAL Team.
+# All rights reserved. See LICENSE for details.
+
 import json
 
 import requests
