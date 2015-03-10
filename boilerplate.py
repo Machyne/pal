32294dc6aa6b2c26a002789334f18d9c0dcc02d4
Alex Simonides <acsimonides@gmail.com>
2015-03-10 00:15:10 -0500

    Add template file containing just the header

---
+++ boilerplate.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Copyright (c) 2015, PAL Team.
+# All rights reserved. See LICENSE for details.
+#
+# 1-2 sentence high-level description of what's going on here
+#
+# Author: Alex Simonides
+#
+# TODO:
+# - Thing 1
+# - Thing 2
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf-8
 #
 # Copyright (c) 2015, PAL Team.
 # All rights reserved. See LICENSE for details.
