b05ad54875cd1d6cc05faafb551d7639366b265b
Greg Fournier <fournieg@carleton.edu>
2015-03-12 17:27:19 -0500

    mysterious missing comments readded

--- pal/services/wa_service.py
+++ pal/services/wa_service.py
@@ -1,4 +1,9 @@
-import urllib
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Copyright (c) 2015, PAL Team.
+# All rights reserved. See LICENSE for details.import urllib
+
 import xml.etree.ElementTree as ET
 
 import requests
